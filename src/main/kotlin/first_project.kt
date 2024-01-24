2eaaf861130b302a3d48917e48e2ca927147a837
Sergey <u3jiom@bk.ru>
2024-01-25 01:29:58 +0400

    "Решение задач"

---
+++ src/main/kotlin/first_project.kt
@@ -0,0 +1,17 @@
+fun main(args: Array<String>){
+    println("Hello programmer")
+
+    print("Введите имя: ")
+    val name = readLine()
+    print("Введите телефон: ")
+    val phone = readLine()
+    print("Введите email: ")
+    val email = readLine()
+    print("Введите адрес: ")
+    val address = readLine()
+
+    println("Ваше имя: $name")
+    println("Ваше имя: $phone")
+    println("Ваш email: $email")
+    println("Ваш адрес: $address")
+}
